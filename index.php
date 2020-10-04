3cbbd515310363813256977a8d49d031c4829fec
GakuA <akazawagaku@gmail.com>
2020-10-04 16:44:26 +0900

    json read

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="spot.json"></script>
 
 		<title>旅立ちの扉</title>
 	</head>
@@ -17,7 +18,7 @@
 
 				$("#door").click(function() {
 					var random = Math.floor( Math.random() * 4 );
-					alert(data[random][0]);
+					alert(place[random][0]);
 
 				});
 
