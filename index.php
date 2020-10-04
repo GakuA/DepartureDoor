8d2976b1eaf79e04766f635c620471166ceccd3b
GakuA <akazawagaku@gmail.com>
2020-10-04 20:27:36 +0900

    clean

--- index.php
+++ index.php
@@ -7,30 +7,18 @@
 		<title>旅立ちの扉</title>
 	</head>
 	<body>
-		<input id="door" type="button" value="旅立つ">
-		<a target="_blank" href="https://www.google.com/maps/@35.7053381,139.7521395,2a,82.7y,323.59h,94.3t/data=!3m6!1e1!3m4!1sJgFRpzWV4XJWSgtPju63Dw!2e0!7i13312!8i6656">test</a>
 		<a id="test" target="_blank" href="/"><input type="button" value="旅立つ"></a>
 		<script>
 			$(function(){
 				var place = "";
 				$.getJSON("spot.json" , function(data) {
-					//alert(data[0][0]);
 					place = data;
 				});
 
 				$("#door").click(function() {
 					var random = Math.floor( Math.random() * 7 );
-					//alert(place[random][0]);
-					window.open(place[random][1], '_blank'); // 新しいタブを開き、ページを表示
-
-				});
-
-				$("#test").click(function() {
-					var random = Math.floor( Math.random() * 7 );
 					$(this).attr("href", place[random][1]);
 				});
-
-
 			});
 		</script>
 	</body>
