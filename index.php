fb3a6ffd6fdfea5d14eca77238f74aa9e0e0404a
GakuA <akazawagaku@gmail.com>
2020-10-04 17:43:16 +0900

    fix

--- index.php
+++ index.php
@@ -4,14 +4,6 @@
 		<meta charset="utf-8">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-		<script>
-		$(function(){
-		$.getJSON("https://departuredoor.herokuapp.com/spot.json" , function(data) {
-		    alert(data[0][0]);
-		  });
-		});
-		</script>
-
 		<title>旅立ちの扉</title>
 	</head>
 	<body>
@@ -27,7 +19,11 @@
 
 				};
 */
-
+				var place = "";
+				$.getJSON("spot.json" , function(data) {
+					//alert(data[0][0]);
+					place = data;
+				});
 
 				$("#door").click(function() {
 					var random = Math.floor( Math.random() * 4 );
