2e36789da08a35362cac1a74d2078fb060c5c153
GakuA <akazawagaku@gmail.com>
2020-10-04 16:20:14 +0900

    jquery

--- index.php
+++ index.php
@@ -2,9 +2,16 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title></title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+		<title>旅立ちの扉</title>
 	</head>
 	<body>
-		testa
+		<input id="door" type="button" value="旅立つ">
+		<script>
+		import json
+		with open('spot.json') as f:
+		data = json.load(f)
+		</script>
 	</body>
 </html>
