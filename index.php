f646ce90de4650140e83c55f0436e6166ee1a57c
GakuA <akazawagaku@gmail.com>
2020-10-09 07:24:34 +0900

    google

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width,initial-scale=1">
 		<meta name="google-site-verification" content="3QuaQCtsYO9KWnRPItMk0t7_q-H5P5n8ZD4LcdcNh2E" />
+
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 		<title>旅立ちの扉</title>
