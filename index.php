217e4645e175849baf99ba7d5953a6aec3540144
GakuA <akazawagaku@gmail.com>
2020-10-11 16:15:30 +0900

    spot

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
 				});
 
 				$("#departure").click(function() {
-					var random = Math.floor( Math.random() * 48 );
+					var random = Math.floor( Math.random() * 55 );
 					$(this).attr("href", place[random][1]);
 				});
 			});
