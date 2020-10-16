a1a0b4fe2f1de13b292ff94f2b5c4f19356d6ba6
GakuA <akazawagaku@gmail.com>
2020-10-17 08:52:24 +0900

    spot

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
 				});
 
 				$("#departure").click(function() {
-					var random = Math.floor( Math.random() * 69 );
+					var random = Math.floor( Math.random() * 73 );
 					$(this).attr("href", place[random][1]);
 				});
 			});
