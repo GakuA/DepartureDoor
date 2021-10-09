72dc8c363a805687c0a564a5825c5d9512b90fc2
gaku akazawa <akazawagaku@gmail.com>
2021-10-10 06:27:53 +0900

    title

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ akazawagaku@gmail.com
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-		<title>旅立ちの扉</title>
+		<title>扉の向こうは日本のどこか</title>
 	</head>
 	<body>
 		<!--<a id="door" target="_blank" href="/"><input type="button" value="旅立つ"></a>-->
